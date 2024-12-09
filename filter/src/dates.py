dc9969a398bb9f905e12963a103866c16f92e65d
Bailey <bailey@hrdag.org>
2024-12-08 22:20:25 -0800

    added backslash to escape characters

--- filter/src/dates.py
+++ filter/src/dates.py
@@ -46,7 +46,7 @@ def get_logger(sname, file_name=None):
 def find_date_complained(line):
     if not line: return None
     clean = line.replace("-", "/")
-    complained = re.findall("[date\sof]*co[\s]*mplain[date:\s]*[a-z0-9\s.,]*:[\s]*([0-9]+\s*\/[0-9]+\/*\s*[0-9]+)", clean, flags=re.I)
+    complained = re.findall("[date\\sof]*co[\\s]*mplain[date:\\s]*[a-z0-9\\s.,]*:[\\s]*([0-9]+\\s*\\/[0-9]+\\/*\\s*[0-9]+)", clean, flags=re.I)
     if not complained: return None
     return complained[0]
 
@@ -54,14 +54,14 @@ def find_date_complained(line):
 def find_date_completed(line):
     if not line: return None
     clean = line.replace("-", "/")
-    completed = re.findall("[date\sofc]*omplet[ion\sdate:]*[a-z0-9\s.,]*:[\s]*([0-9]+[\s\/;]*[0-9]+[\s\/]*[0-9]+)", clean, flags=re.I)
+    completed = re.findall("[date\\sofc]*omplet[ion\\sdate:]*[a-z0-9\\s.,]*:[\\s]*([0-9]+[\\s\\/;]*[0-9]+[\\s\\/]*[0-9]+)", clean, flags=re.I)
     if not completed: return None
     return completed[0]
 
 
 def find_mediation_info(line):
     if not line: return None
-    mediated = re.findall("(mediated\s*[a-z0-9\-,\s/]*resolved.*$)", line, flags=re.I)
+    mediated = re.findall("(mediated\\s*[a-z0-9\\-,\\s/]*resolved.*$)", line, flags=re.I)
     if not mediated: return None
     return mediated
 #}}}
@@ -74,7 +74,7 @@ if __name__ == '__main__':
     # arg handling
     args = get_args()
     logger.info('loading data')
-    allegs = pd.read_parquet(args.input, columns=['allegation_id', 'complaint_meta', 'allegation_text'])    
+    allegs = pd.read_parquet(args.input, columns=['allegation_id', 'complaint_meta', 'allegation_text'])
     logger.info('adding DATE OF COMPLAINT as date_complained')
     allegs['date_complained'] = allegs.complaint_meta.apply(
         find_date_complained)
@@ -87,16 +87,16 @@ if __name__ == '__main__':
     allegs['mediation_status'] = allegs.allegation_text.apply(
         find_mediation_info)
     allegs.mediation_status.sample(5)
-    
+
     miss_complain = allegs.date_complained.isna().sum()
     miss_complete = allegs.date_completed.isna().sum()
     logger.info(f'missing date_complained (count):\t{miss_complain}')
     logger.info(f'missing date_completed (count):\t{miss_complete}')
     assert allegs.date_complained.isna().sum() < (allegs.shape[0]*.5)
-    assert allegs.mediation_status.notna().sum() > 400 
+    assert allegs.mediation_status.notna().sum() > 400
     allegs.drop(columns=['allegation_text', 'complaint_meta'], inplace=True)
     allegs.to_parquet(args.output)
     logger.info("done.")
-    
+
 #}}}
 # done.
